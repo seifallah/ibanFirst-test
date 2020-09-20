f693fe030fda7d39d049dc8c41e9ee26a537f1f0
seifallah <seifallah.benamer@gmail.com>
2020-09-20 21:29:35 +0100

    added 2 tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,23 @@
+import AccountList from './account/AccountList';
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import { createMount  } from '@material-ui/core/test-utils';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  let mount, wrapper;
+  beforeEach(() => {
+   mount = createMount();
+   wrapper = mount(<AccountList />);
+  });
+
+  it('should not contains an error', () => {
+    const error = wrapper.containsMatchingElement(".errorContainer");
+    expect(error).toBe(false);
+  });
+
+  it('should have default value EUR', () => {
+    const item = 'EUR';
+    const toggleTitle = wrapper.find("input").find('.MuiSelect-nativeInput').props().value;
+    expect(toggleTitle).toEqual(item);
+  });
+
 });
